de402b91210ea32c26a2dbb7d7acb6122a9f5643
Benjamin <134766715+Wilki406@users.noreply.github.com>
2024-04-21 21:55:28 +1000

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ app = CTk()
 app.geometry("500x500")
 data = 'dataforsat.csv'
 records = []
+showstate = "*"
 
 with open(data, 'r') as file:
     reader = csv.reader(file)
@@ -19,33 +20,36 @@ with open(data, 'r') as file:
 
 print(records)
 
-def click_handler():
-    if entry.get() == "test":
-        print("TESTINGG")
+def logHandler():
+    for i, row in enumerate(records):
+        if entry.get() == row[1]:
+            print("TESTINGG")
 
-frame = CTkFrame(master=app, width=400, height=320, fg_color="#9B9B9B")
+def hiddenHandler():
+    global showstate
+    showstate = rad.get()
+    entry2.configure(show=showstate)
+
+frame = CTkFrame(master=app, width=400, height=300, fg_color="#9B9B9B")
 frame.pack_propagate(0)
 frame.pack(expand=True)
 
-label = CTkLabel(master=frame, text="Welcome! Sign in", text_color="black")
+label = CTkLabel(master=frame, text="Welcome!", text_color="black", font=('Cooper Black', 50))
 label.pack(anchor="n", expand=True)
 
-labelu = CTkLabel(master=frame, text="Username", text_color="black")
-labelu.pack(anchor="s", expand=True)
+labelu = CTkLabel(master=frame, text="Enter details to log in", text_color="black")
+labelu.pack(anchor="n", expand=True)
 
-entry = CTkEntry(master=frame, placeholder_text="Enter Username")
+entry = CTkEntry(master=frame, placeholder_text="Username")
 entry.pack(anchor="n", expand=True)
 
-labelp = CTkLabel(master=frame, text="Password", text_color="black")
-labelp.pack(anchor="s", expand=True,)
-
-entry2 = CTkEntry(master=frame, placeholder_text="Enter Password", show="*")
+entry2 = CTkEntry(master=frame, placeholder_text="Password", show=showstate)
 entry2.pack(anchor="n", expand=True)
 
-rad = CTkCheckBox(master=frame,border_color="black", text="Show password")
-rad.pack(anchor="center", side="right")
+rad = CTkCheckBox(master=frame,border_color="black", text="Show password", command=hiddenHandler, onvalue="", offvalue="*")
+rad.pack(side="right",padx=50, pady=20)
 
-btn = CTkButton(master=frame, text="Submit", command=click_handler)
-btn.pack(anchor='center', expand=True)
+btn = CTkButton(master=frame, text="Submit", command=logHandler)
+btn.pack(expand=True,padx=50, pady=20, side="left")
 
 app.mainloop()
