776d1b572893bd8af07cc73e0cfa0deca5c8b16a
Benjamin <134766715+Wilki406@users.noreply.github.com>
2024-05-05 21:20:52 +1000

    Update SignInPage.py

--- SignInPage.py
+++ SignInPage.py
@@ -4,7 +4,6 @@ import csv
 
 data = 'dataforsat.csv'
 records = []
-showstate = "*"
 
 with open(data, 'r') as file:
     reader = csv.reader(file)
@@ -12,11 +11,14 @@ with open(data, 'r') as file:
     for row in reader:
         ids = row[0]
         usernames = row[1]
-        pwords = row[2]
+        passwords = row[2]
+        firstNames = row[3]
+        lastNames = row[4]
 
-        records.append([ids, usernames, pwords])
+        records.append([ids, usernames, passwords, firstNames, lastNames])
 
 print(records)
+
 
 class MainPage(customtkinter.CTk):
     def __init__(self):
@@ -24,25 +26,33 @@ class MainPage(customtkinter.CTk):
         self.title("Inventory Tracker")
         self.geometry("1920x1080")
 
-
 class SignIn(customtkinter.CTkToplevel):
-    def __init__(self, app):
+    def __init__(self, signApp):
         super().__init__()
-        self.app = app
+        self.app = signApp
         self.title("Sign In")
-        self.geometry("735x405")
+        self.geometry("700x400")
+        self.resizable(width=False, height=False)
+
+        showstate = "*"
 
         def hidHandler():
             global showstate
             showstate = self.rad.get()
-            self.entry2.configure(show=showstate)
+            self.passwordEntry.configure(show=showstate)
 
         def logHandler():
             for row in records:
-                if self.entry.get() == row[1]:
-                    print("TESTINGG")
-                    if self.entry2.get() == row[2]:
-                        print("Sign in Successful")
+                if self.userEntry.get() == row[1] and self.passwordEntry.get() == row[2]:
+                        print(f"Sign in Successful")
+
+                        #
+                        # MAKE THIS PRINT SAY THE NAME AND LAST NAME
+                        #
+
+
+                        
+
                         self.withdraw()  # hide the SignIn window
                         self.app.deiconify()  # show the MainPage window
                         return
@@ -55,46 +65,88 @@ class SignIn(customtkinter.CTkToplevel):
         self.frame = CTkFrame(self, width=500, height=400, fg_color="#9B9B9B")
         self.frame.grid()
 
-        self.label = CTkLabel(self.frame, text="Welcome!", text_color="black", font=('Cooper Black', 50))
-        self.label.grid(column=1, row=1, padx=(250,250),pady=(50,0), columnspan=4)
+        self.label = CTkLabel(self.frame, text="Welcome!", text_color="black", font=('Berlin Sans FB', 50))
+        self.label.grid(column=1, row=1, padx=(250, 250), pady=(40, 0), columnspan=4)
 
-        self.labelu = CTkLabel(self.frame, text="Enter details to log in", text_color="black")
-        self.labelu.grid(column=1,row=2, columnspan=4, pady=(0,50))
+        self.labelu = CTkLabel(self.frame, text="Enter details to log in", text_color="black", font=('Berlin Sans FB', 20))
+        self.labelu.grid(column=1, row=2, columnspan=4, pady=(0, 50))
 
-        self.entry = CTkEntry(self.frame, placeholder_text="Username",height=30, width=280)
-        self.entry.grid(column=1,row=3, columnspan=4,pady=(0,10))
+        self.userEntry = CTkEntry(self.frame, placeholder_text="Username", height=30, width=280)
+        self.userEntry.grid(column=1, row=3, columnspan=4, pady=(0, 10))
 
-        self.entry2 = CTkEntry(self.frame, placeholder_text="Password", show=showstate, height=30, width=280)
-        self.entry2.grid(column=1, row=4, columnspan=4,pady=(0,100))
+        self.passwordEntry = CTkEntry(self.frame, placeholder_text="Password", show=showstate, height=30, width=280)
+        self.passwordEntry.grid(column=1, row=4, columnspan=4, pady=(0, 10))
 
-        self.btn = CTkButton(self.frame, text="Submit", command=logHandler)
-        self.btn.grid(column=1,row=6, columnspan=2, pady=(5,15))
+        self.rad = CTkCheckBox(self.frame, border_color="black", text="Show password", command=hidHandler, onvalue="",
+                               offvalue="*")
+        self.rad.grid(column=1, row=5, columnspan=4, pady=(5, 65))
+
+        self.btn = CTkButton(self.frame, text="Login", command=logHandler, fg_color="#30a474", hover_color="#1c5c41")
+        self.btn.grid(column=1, row=6, columnspan=4, pady=(5, 20), padx=(50, 230))
 
         self.btn = CTkButton(self.frame, text="Register", command=regHandler)
-        self.btn.grid(column=2, row=6, columnspan=2, pady=(5,15))
+        self.btn.grid(column=2, row=6, columnspan=4, pady=(5, 20), padx=(50, 50))
 
-        self.rad = CTkCheckBox(self.frame, border_color="black", text="Show password", command=hidHandler, onvalue="", offvalue="*")
-        self.rad.grid(column=3, row=6, columnspan=3, pady=(5,15))
 
 class Registry(customtkinter.CTkToplevel):
     def __init__(self, sign_in_window):
         super().__init__()
+        newUserData = []
+        placeholdercolor = "Grey"
         self.sign_in_window = sign_in_window
 
-        self.frame2 = CTkFrame(self, width=500, height=300, fg_color="#9B9B9B")
-        self.frame2.pack(expand=True)
+        self.frame = CTkFrame(self, width=500, height=400, fg_color="#9B9B9B")
+        self.frame.grid()
 
         def regBack():
             self.withdraw()
             self.sign_in_window.deiconify()
 
+        def regSignup():
+            try:
+                if self.nameEntry.get() and self.usernameEntry.get() and self.passwordEntry.get() and self.rePasswordEntry.get() != "":
+                    fullname = self.nameEntry.get()
+                    username = self.usernameEntry.get()
+                    firstPassword = self.passwordEntry.get()
+                    secondPassword = self.rePasswordEntry.get()
+
+                    if firstPassword == secondPassword:
+                        print("yay")
+
+                    #newUserData.append([ids, usernames, passwords, firstNames, lastNames])
+
+            except ValueError:
+                print("Invalid input: Please enter an integer.")
+
+
+
         self.title("Register")
-        self.geometry("800x200")
+        self.geometry("700x400")
 
-        self.btn = CTkButton(self.frame2, text="Back", command=regBack)
-        self.btn.grid(pady=5)
+        self.label = CTkLabel(self.frame, text="Create an account", text_color="black", font=('Berlin Sans FB', 30))
+        self.label.grid(column=1, row=1, padx=(250, 250), pady=(30, 25), columnspan=2)
 
+        self.nameEntry = CTkEntry(self.frame, placeholder_text="Your full name", height=30, width=280, placeholder_text_color=placeholdercolor)
+        self.nameEntry.grid(column=1, row=2, columnspan=2, pady=(0, 10))
 
+        self.usernameEntry = CTkEntry(self.frame, placeholder_text="Username", height=30, width=280, placeholder_text_color=placeholdercolor)
+        self.usernameEntry.grid(column=1, row=3, columnspan=2, pady=(0, 10))
+
+        self.passwordEntry = CTkEntry(self.frame, placeholder_text="Password", height=30, width=280, placeholder_text_color=placeholdercolor)
+        self.passwordEntry.grid(column=1, row=4, columnspan=4, pady=(0, 10))
+
+        self.rePasswordEntry = CTkEntry(self.frame, placeholder_text="Re-enter password", height=30, width=280, placeholder_text_color=placeholdercolor)
+        self.rePasswordEntry.grid(column=1, row=5, columnspan=4, pady=(0, 10))
+
+        self.createBtn = CTkButton(self.frame, text="Create account", command=regSignup)
+        self.createBtn.grid(column=1, row=6, columnspan=2, pady=(5, 15))
+
+        self.label = CTkLabel(self.frame, text="Already got an account?", text_color="black",
+                              font=('Berlin Sans FB', 20))
+        self.label.grid(column=1, row=7, padx=(250, 250), pady=(0, 15), columnspan=2)
+
+        self.btn = CTkButton(self.frame, text="Login", command=regBack)
+        self.btn.grid(column=1, row=8, padx=(250, 250), pady=(0, 50), columnspan=2)
 
 app = MainPage()
 SignInWindow = SignIn(app)
